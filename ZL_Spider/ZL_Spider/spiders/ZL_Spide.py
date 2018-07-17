d99e5fc0ae3bda9cae9361ca2f9faf4e4becd343
liangxs0 <1033808656@qq.com>
2018-07-17 16:24:28 +0800

    updata

--- ZL_Spider/ZL_Spider/spiders/ZL_Spide.py
+++ ZL_Spider/ZL_Spider/spiders/ZL_Spide.py
@@ -3,16 +3,17 @@ import  scrapy
 
 class ZL_Spider(scrapy.Spider):
     name = 'ZL'
-    start_urls = ['https://sou.zhaopin.com/jobs/searchresult.ashx?bj=160000&sj=079&in=160400&jl=%E5%8C%97%E4%BA%AC&kw=python&p=1&isadv=0']
+    start_urls = ['https://sou.zhaopin.com/jobs/searchresult.ashx?in=160400%3B160000&jl=%E5%8C%97%E4%BA%AC&kw=python%E5%BC%80%E5%8F%91&p=1&isadv=0']
     def parse(self, response):
         companyName = response.xpath('//td/a[@target="_blank"]/text()')
         print(companyName.extract())
         companyAddress = response.css('[class="gzdd"]::text')
         companyAddress.pop(0)
         print(companyAddress.extract())
-        Len1 = len(companyName)
-        Len2 = len(companyAddress)
-        print(Len1,Len2)
-
-
-
+        salary = response.css('[class="zwyx"]::text')
+        salary.pop(0)
+        print(salary.extract())
+        release_time = response.xpath('//td[@class="gxsj"]/span/text()')
+        print(release_time.extract())
+        job_requirements = response.xpath('//li[@class="newlist_deatil_last"]/text()')
+        print(job_requirements.extract())
